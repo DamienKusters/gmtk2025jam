25d6025a7e7583787a1a6edcdfc4a3bcaeb6f231
Damien Kusters <damien.kusters@ziggo.nl>
2025-08-03 14:34:53 +0200

    Cleanups

--- game.gd
+++ game.gd
@@ -4,9 +4,9 @@ extends Node2D
 
 func _ready():
 	for coord in Global.restocking_coords:
-		var i = Global.get_restocking_point_instance()
-		i.global_position = coord
-		$RestockingPoints.add_child(i)
+		var x = Global.get_restocking_point_instance()
+		x.global_position = coord
+		$RestockingPoints.add_child(x)
 	
 	var i = Global.get_selected_racetrack_instance()
 	i.player = $Player
